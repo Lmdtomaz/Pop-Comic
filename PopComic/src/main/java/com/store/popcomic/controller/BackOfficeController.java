980127a3e2b7b26ba8da6c7be231a7829de76b88
leandro <lmdtomaz@gmail.com>
2024-11-20 12:30:25 -0300

    Cadastro Usuario/ Login com Sessao/ Tela backoffice

---
+++ PopComic/src/main/java/com/store/popcomic/controller/BackOfficeController.java
@@ -0,0 +1,39 @@
+package com.store.popcomic.controller;
+
+import com.store.popcomic.model.Usuario;
+import com.store.popcomic.repository.UsuarioRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/backoffice")
+public class BackOfficeController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @GetMapping("areaadm")
+    public ModelAndView backOffice(HttpSession session) {
+        ModelAndView mv = new ModelAndView();
+
+        // Verifica se há um usuário logado na sessão
+        Usuario usuarioLogado = (Usuario) session.getAttribute("usuarioLogado");
+        if (usuarioLogado != null) {
+            // Usuário logado, exibe a página de backoffice
+            mv.setViewName("backoffice");
+            mv.addObject("usuario", usuarioLogado);
+        } else {
+            // Redireciona para a tela de login caso não haja usuário logado
+            mv.setViewName("redirect:/adm/usuario/loginAdm");
+        }
+
+        return mv;
+    }
+
+}
+
